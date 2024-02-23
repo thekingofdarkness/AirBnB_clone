188589038a180ee85cfb239f0fde6d6c123fa14d
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2024-02-23 13:43:22 -0800

    auto

--- models/amenity.py
+++ models/amenity.py
@@ -5,11 +5,4 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """User calss"""
-    name = ""#!/usr/bin/python3
-"""module file"""
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """User calss"""
     name = ""
